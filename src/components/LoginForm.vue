a9bee25d21d5ed180c50b1a0ecad619d0d0632d0
hyojeongyun <ynhyojng@gmail.com>
2023-02-06 16:31:02 +0900

    학습 노트 데이터 생성

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -32,7 +32,6 @@
 </template>
 
 <script>
-import { loginUser } from '@/api/index';
 import { validateEmail } from '@/utils/validation';
 
 export default {
@@ -57,9 +56,7 @@ export default {
 					username: this.username,
 					password: this.password,
 				};
-				const { data } = await loginUser(userData);
-				this.$store.commit('setToken', data.token);
-				this.$store.commit('setUsername', data.user.username);
+				await this.$store.dispatch('LOGIN', userData);
 				this.$router.push('/main');
 			} catch (error) {
 				this.logMessage = error.response.data;
